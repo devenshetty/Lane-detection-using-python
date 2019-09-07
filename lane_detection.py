8cf4e6cbd4f818ad6b2159b99346c8cbb352721f
devenshetty <55019078+devenshetty@users.noreply.github.com>
2019-09-07 15:41:40 +0530

    Update lane_detection.py

--- lane_detection.py
+++ lane_detection.py
@@ -4,15 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def canny(lane_image):
-  ''' In this function , the image is converted into a gray scale image. This is followed by median blurring
-  to smoothen the image. The final operation is canny edge detection and the reultant image is returned.'''
+ 
   gray=cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
   blur=cv2.medianBlur(gray, 5)
   canny= cv2.Canny(blur , 50 , 150 )
   return canny
 
 def display(image, lines):
-  '''In this function the detected lines are drwan on the original image(i.e draw lane lines) '''
+ 
   
   if lines is not None:
     for line in lines:
@@ -21,10 +20,7 @@ def display(image, lines):
   return lane_image
 
 def interest(lane_image):
-  ''' In this function the first operation includes the location of the co-ordinate ppoints which in this case is a triangle.
-  The co-ordinates were located by plotting using matplotlib.pyplot(not a part of the main code). A dark image was then superimposed with the triangular area
-  using the fillPoly command. The masked image was obtained by performing bit wise and operation between the canny edged image and the superimposed image.
-  White corresponds to 1 and black corresponds to zero. The masked image was returned.'''
+  
   area=np.array([[(200,700) , (1100,700) , (550,250)]])
   mask = np.zeros_like(lane_image)
   cv2.fillPoly(mask,area , 255)  
